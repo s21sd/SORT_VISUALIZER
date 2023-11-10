85bc0803e1b76a17d537748b2687d9b3d5eba54c
Sunny Srivastava <98369603+s21sd@users.noreply.github.com>
2023-11-11 03:35:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function insertionSort() {
     // console.log("working");
 }
 function bubbleSort() {
-    window.location.href = 'BubbleSort.html'
+    window.location.href = 'https://s21sd.github.io/SORT_VISUALIZER/BubbleSort.html'
     // console.log("working");
 }
 function quickSort() {
