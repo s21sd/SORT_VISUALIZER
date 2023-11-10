afd94df3f5e9c16212863c4076bf0f1ae46e7fd0
Sunny Srivastava <98369603+s21sd@users.noreply.github.com>
2023-11-11 03:36:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function selectionSort() {
-    window.location.href = 'http://127.0.0.1:5500/SelectionSort.html'
+    window.location.href = 'https://s21sd.github.io/SORT_VISUALIZER/SelectionSort.html'
     // console.log("working");
 }
 function insertionSort() {
-    window.location.href = 'http://127.0.0.1:5500/InsertionSort.html'
+    window.location.href = 'https://s21sd.github.io/SORT_VISUALIZER/InsertionSort.html'
     // console.log("working");
 }
 function bubbleSort() {
@@ -11,10 +11,10 @@ function bubbleSort() {
     // console.log("working");
 }
 function quickSort() {
-    window.location.href = 'http://127.0.0.1:5500/QuickSort.html'
+    window.location.href = 'https://s21sd.github.io/SORT_VISUALIZER/QuickSort.html'
     // console.log("working");
 }
 function mergeSort() {
-    window.location.href = 'http://127.0.0.1:5500/MergerSort.html'
+    window.location.href = 'https://s21sd.github.io/SORT_VISUALIZER/MergerSort.html'
     // console.log("working");
 }
